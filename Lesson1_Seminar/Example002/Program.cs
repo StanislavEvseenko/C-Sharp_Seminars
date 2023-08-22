dbb4447daec9335b4caabd31c510fd49c9725fe8
Stanislav <stanislav.evs.i@yandex.ru>
2023-08-22 21:31:09 +0300

    add Example002

---
+++ Lesson1_Seminar/Example002/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число 1: ");
+string numA = Console.ReadLine()!;
+int a = int.Parse(numA);
+Console.Write("Введите число 2: ");
+string numB = Console.ReadLine()!;
+int b = int.Parse(numB);
+
+if(a == b * b)
+    {
+        Console.WriteLine($"Число {a} является квадратом числа {b}");
+    }
+else
+    {
+        Console.WriteLine($"Число {a} не является квадратом числа {b}");
+    }
