2b7f0b177be6f071c1f14c8c57e22b71856792ef
Stanislav <stanislav.evs.i@yandex.ru>
2023-08-22 21:45:00 +0300

    add Example003

---
+++ Lesson1_Seminar/Example003/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Введите номер дня недели: ");
+string num = Console.ReadLine()!;
+int day = int.Parse(num);
+
+switch (day)
+{
+    case 1:
+        Console.WriteLine("Понедельник");
+        break;
+    case 2:
+        Console.WriteLine("Вторник");
+        break;
+    case 3:
+        Console.WriteLine("Среда");
+        break;
+    case 4:
+        Console.WriteLine("Четверг");
+        break;
+    case 5:
+        Console.WriteLine("Пятница");
+        break;
+    case 6:
+        Console.WriteLine("Суббота");
+        break;
+    case 7:
+        Console.WriteLine("Воскресенье");
+        break;
+    default:
+        Console.WriteLine("Такого дня не существует!");
+        break;
+};
