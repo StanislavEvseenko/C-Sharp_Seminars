f3081430df4afb86996e60939de3dda28e0301fd
Stanislav <stanislav.evs.i@yandex.ru>
2023-08-22 21:16:48 +0300

    add Example001

---
+++ Lesson1_Seminar/Example001/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите число: ");
+string num = Console.ReadLine()!;
+int number = int.Parse(num);
+
+int square = number * number;
+Console.WriteLine($"Квадрат числа {number} равен {square}");
