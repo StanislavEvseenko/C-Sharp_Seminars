78f0cb296fcd0af47689f4657eba764071cfcf9d
Stanislav <stanislav.evs.i@yandex.ru>
2023-08-27 16:32:39 +0300

    add Example005

---
+++ Lesson1_Seminar/Example005/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.Write("Введите трехзначное число: ");
+string num = Console.ReadLine()!;
+int number = int.Parse(num);
+int lastNum = (number % 10);
+Console.WriteLine(lastNum);
