3b1640c23859ca2864b59c694c16c49ef69d8472
Stanislav <stanislav.evs.i@yandex.ru>
2023-08-27 16:45:26 +0300

    add HW1

--- Lesson1_HW/HW001/Program.cs
+++ Lesson1_HW/HW001/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+string num1 = Console.ReadLine()!;
+int number1 = int.Parse(num1);
+Console.Write("Введите второе число: ");
+string num2 = Console.ReadLine()!;
+int number2 = int.Parse(num2);
+
+if (number1 > number2)
+{
+    Console.WriteLine($"{number1} больше чем {number2}");
+}
+else
+{
+    Console.WriteLine($"{number2} больше чем {number1}");
+};
