4f221110bd008a8a1557c28af22adea5fb30797f
Stanislav <stanislav.evs.i@yandex.ru>
2023-08-27 18:29:30 +0300

    add Example1

---
+++ Lesson2_Seminar/Example001/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+int MaxDigit(int num)
+{
+    if (num / 10 > num % 10)
+        return num / 10;
+    else
+        return num % 10;
+}
+
+int number = new Random().Next(10, 100); // [a, b)
+int res = MaxDigit(number);
+Console.WriteLine($"В числе {number} максимальная цифра {res}");
