ec3f0baa474c3c00b47c04832c8f5d7a01cc0e0a
Stanislav <stanislav.evs.i@yandex.ru>
2023-08-27 17:00:05 +0300

    add HW4

---
+++ Lesson1_HW/HW004/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+string num = Console.ReadLine()!;
+int number = int.Parse(num);
+int count = 1;
+
+ if (count < number)
+ {
+    while(count <= number)
+    {
+        if (count % 2 == 0)
+        {
+        Console.WriteLine(count);
+        };
+    count++;
+    };
+ }
+ else
+ {
+    while(number <= count)
+    {
+        if (number % 2 == 0)
+        {
+        Console.WriteLine(number);
+        };
+    number++;
+    };
+ }
