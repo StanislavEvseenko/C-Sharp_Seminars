a22b92cd6c999904883e57011224b5914634c83e
Stanislav <stanislav.evs.i@yandex.ru>
2023-08-27 16:51:08 +0300

    add HW2

---
+++ Lesson1_HW/HW002/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+string num1 = Console.ReadLine()!;
+int number1 = int.Parse(num1);
+Console.Write("Введите второе число: ");
+string num2 = Console.ReadLine()!;
+int number2 = int.Parse(num2);
+Console.Write("Введите третье число: ");
+string num3 = Console.ReadLine()!;
+int number3 = int.Parse(num3);
+int max = 0;
+
+if (number1 > number2)
+{
+    max = number1;
+}
+else
+{
+    max = number2;
+};
+if (number3 > max)
+{
+    max = number3;
+}
+
+Console.WriteLine(max);
