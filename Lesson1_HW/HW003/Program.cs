ef9654dcd19647b5d9047f6125da7e954b9e54fd
Stanislav <stanislav.evs.i@yandex.ru>
2023-08-27 16:54:12 +0300

    add HW3

---
+++ Lesson1_HW/HW003/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+string num = Console.ReadLine()!;
+int number = int.Parse(num);
+
+
+if ((number % 2) == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+};
