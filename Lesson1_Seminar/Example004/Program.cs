f9ac560e255a82922db1aabb49e5cdae1a670358
Stanislav <stanislav.evs.i@yandex.ru>
2023-08-27 16:28:47 +0300

    add Example004

---
+++ Lesson1_Seminar/Example004/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+string num = Console.ReadLine()!;
+int number = int.Parse(num);
+int count = number * (-1);
+
+ while(count <= number)
+ {
+    if (count % 2 == 0)
+    {
+       Console.WriteLine(count);
+    };
+    count++;
+};
