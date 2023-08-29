b5fa5641f96526da70363e51e090663381a25ac8
Stanislav <stanislav.evs.i@yandex.ru>
2023-08-29 21:17:21 +0300

    add Example2

---
+++ Lesson2_Seminar/Example002/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+int DelSecondDigit(int number)
+{
+    int firstDig = number / 100;
+    int secondDig = number % 10;
+
+    int result = firstDig * 10 + secondDig;
+    return result;
+}
+
+int randomNum = new Random().Next(100, 1000);
+
+Console.WriteLine($"{randomNum} --> {DelSecondDigit(randomNum)}");
