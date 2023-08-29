6063a7f9df111ebbd794fdb564881b7c64977641
Stanislav <stanislav.evs.i@yandex.ru>
2023-08-29 22:23:17 +0300

    add Example4

---
+++ Lesson2_Seminar/Example004/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+bool Test(int number)
+{
+    return number % 7 == 0 && number % 23 == 0;
+}
+
+int num = InputNum("Введите число: ");
+
+Console.WriteLine(Test(num));
