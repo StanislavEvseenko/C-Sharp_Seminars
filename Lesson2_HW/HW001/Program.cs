31afc5db8b83c1888035ee4c042f3b4725cb507f
Stanislav <stanislav.evs.i@yandex.ru>
2023-09-19 20:34:55 +0300

    HW Lesson 2

---
+++ Lesson2_HW/HW001/Program.cs
@@ -0,0 +1,27 @@
+﻿//найти вторую цифру числа
+
+Console.Clear();
+
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+void IsSquare(int number1, int number2)
+{
+    if (number1 * number1 == number2)
+    {
+        Console.WriteLine("Является");
+    }
+    else
+    {
+        Console.WriteLine("Не является");
+    }
+}
+
+int num1 = InputNum("Введите первое число: ");
+int num2 = InputNum("Введите второе число: ");
+
+IsSquare(num1, num2);
