c1f6c7f3f1cde6cb3cb3e8ebf1c4acb4e3dfa973
Stanislav <stanislav.evs.i@yandex.ru>
2023-09-24 16:35:21 +0300

    add HW01

---
+++ Lesson3_HW/HW001/Program.cs
@@ -0,0 +1,24 @@
+﻿//Метод должен проверить является ли число пятизначным, в противном случае - вывести Число не пятизначное и False в следующей строке.
+
+Console.Clear();
+
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+void IsPalindrome(int number)
+{
+    if (number.ToString().Length==5) {
+        if (((number / 1000) % 10 == (number % 100) / 10) && (number/10000 == number % 10)) Console.WriteLine("Полиндром");
+        else Console.WriteLine("Не полиндром");
+    } else {
+        Console.WriteLine("Число не пятизначное");
+    }
+}
+
+int num = InputNum("Введите число: ");
+
+IsPalindrome(num);
