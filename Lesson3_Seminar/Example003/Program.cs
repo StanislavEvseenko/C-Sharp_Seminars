175e36c963c24131b79983c9ba2e4bd099e8749e
Stanislav <stanislav.evs.i@yandex.ru>
2023-09-24 14:52:17 +0300

    add Example03

---
+++ Lesson3_Seminar/Example003/Program.cs
@@ -0,0 +1,27 @@
+﻿//принимает на вход X & Y не равные нулю, и выдает расстояние между ними
+
+Console.Clear();
+
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+double PointDistance(int aX, int aY, int bX, int bY)
+{
+    double dist = 0;
+    int aXY = aX - aY;
+    int bXY = bX - bY;
+
+    dist = Math.Sqrt(Math.Pow(aXY, 2) + Math.Pow(bXY, 2));
+    return dist;
+}
+
+int aX = InputNum("Ввести координату aX: ");
+int aY = InputNum("Ввести координату aY: ");
+int bX = InputNum("Ввести координату bX: ");
+int bY = InputNum("Ввести координату bY: ");
+
+Console.WriteLine(PointDistance(aX, aY, bX, bY));
