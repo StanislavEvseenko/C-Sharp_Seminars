8596a850d5af23fc385d01d9c8d1965edcecc5bb
Stanislav <stanislav.evs.i@yandex.ru>
2023-09-24 14:04:13 +0300

    add Example02

---
+++ Lesson3_Seminar/Example002/Program.cs
@@ -0,0 +1,36 @@
+﻿//принимает на вход X & Y не равные нулю, и выдает номер четверти плоскости в которой эта точка
+
+Console.Clear();
+
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+void QuarterAvaliable(int quarter)
+{
+    if (quarter == 1)
+    {
+        Console.WriteLine("x > 0, y > 0");
+    }
+    else if (quarter == 2)
+    {
+        Console.WriteLine("x < 0, y > 0");
+    }
+    else if (quarter == 3)
+    {
+        Console.WriteLine("x < 0, y < 0");
+    }
+    else if (quarter == 4)
+    {
+        Console.WriteLine("x > 0, y < 0");
+    }
+    else {
+        System.Console.WriteLine("Такой четверти нет");
+    }
+}
+
+int quarter = InputNum("Ввести номер четверти: ");
+QuarterAvaliable(quarter);
