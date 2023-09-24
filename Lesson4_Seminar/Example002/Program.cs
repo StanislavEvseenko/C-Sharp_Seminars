a20940afd30141e02bd0a79ec715f8c396cc422c
Stanislav <stanislav.evs.i@yandex.ru>
2023-09-24 18:38:18 +0300

    add Ex2

---
+++ Lesson4_Seminar/Example002/Program.cs
@@ -0,0 +1,17 @@
+﻿//количество знаков в числе
+
+Console.Clear();
+
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+int num = InputNum("Введите число: ");
+int i;
+for (i = 0; num != 0; i++)
+    num /= 10;
+
+Console.WriteLine(i);
