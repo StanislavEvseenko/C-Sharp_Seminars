0add5a32ce039127439e0dd0e3c58e8269061d29
Stanislav <stanislav.evs.i@yandex.ru>
2023-09-24 19:09:11 +0300

    add Ex3

---
+++ Lesson4_Seminar/Example003/Program.cs
@@ -0,0 +1,23 @@
+﻿//количество знаков в числе
+
+Console.Clear();
+
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+int Factorial(int num)
+{
+    int mul = 1;
+    for(int i = 2; i <= num; i++)
+        mul *= i;
+    return mul;
+}
+
+int num = InputNum("Введите число: ");
+
+int fact = Factorial(num);
+System.Console.WriteLine(fact);
