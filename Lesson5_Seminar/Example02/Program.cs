0d5e217d035be02f05f60d1e9b10eee225034087
Stanislav <stanislav.evs.i@yandex.ru>
2023-10-05 20:39:14 +0300

    add Ex2

---
+++ Lesson5_Seminar/Example02/Program.cs
@@ -0,0 +1,43 @@
+﻿//изменить значения в массиве на противоположные по знаку
+
+Console.Clear();
+
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+void FillArray(int[] araay)
+{
+    Random rnd = new Random();
+    for(int i = 0; i < araay.Length; i++)
+    {
+        araay[i] = rnd.Next(-9, 10);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    };
+    Console.WriteLine();
+}
+
+void ChangeSign(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] *= -1;
+    }
+}
+
+int[] newArray = new int[12];
+
+FillArray(newArray);
+PrintArray(newArray);
+ChangeSign(newArray);
+PrintArray(newArray);
