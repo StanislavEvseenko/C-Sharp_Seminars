9d4bf20c65de807793ae344cc3a232964d561dba
Stanislav <stanislav.evs.i@yandex.ru>
2023-10-05 16:24:46 +0300

    add HW02

---
+++ Lesson4_HW/HW02/Program.cs
@@ -0,0 +1,21 @@
+﻿//сумма знаков в числе
+
+Console.Clear();
+
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+int num = InputNum("Введите число: ");
+int i;
+int sum = 0;
+for (i = 0; num != 0; i++)
+{
+    sum += num % 10;
+    num /= 10;
+}
+
+Console.WriteLine(sum);
