1af1cf79bdd9d156a8ec3052358a83bbcfd542be
Stanislav <stanislav.evs.i@yandex.ru>
2023-10-05 15:50:15 +0300

    add HW01

---
+++ Lesson4_HW/HW01/Program.cs
@@ -0,0 +1,29 @@
+﻿//цикл в котором А в степени В
+
+Console.Clear();
+
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+int a = InputNum("Введите число A: ");
+int b = InputNum("Введите число B: ");
+
+//for (int i = 0; i < b; i++)
+//    a *= a;
+//Console.WriteLine(i);
+
+void Exponentiation(int a, int b)
+{
+    int constA = a;
+    for(int i = 1; i < b; i++)
+    {
+        a *= constA;
+    }
+    Console.WriteLine(a);
+}
+
+Exponentiation(a, b);
