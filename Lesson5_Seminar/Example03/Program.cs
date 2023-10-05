c65ee868e9cbdd229b47c3ccff60b2349a4ce8a8
Stanislav <stanislav.evs.i@yandex.ru>
2023-10-05 21:11:53 +0300

    add Ex3

---
+++ Lesson5_Seminar/Example03/Program.cs
@@ -0,0 +1,54 @@
+﻿//узнать есть-ли в массиве нужное число
+
+Console.Clear();
+
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+void FillArray(int[] araay)
+{
+    Random rnd = new Random();
+    for(int i = 0; i < araay.Length; i++)
+    {
+        araay[i] = rnd.Next(0, 100);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    };
+    Console.WriteLine();
+}
+
+void FindNum(int[] array, int num)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == num)
+        {
+            count++;
+        }
+    };
+    if (count == 0)
+    {
+        Console.WriteLine("нет");
+    } else
+    {
+        Console.WriteLine("да");
+    }
+}
+
+int myNum = InputNum("Введите нужное число от 0 до 100: ");
+int[] newArray = new int[12];
+
+FillArray(newArray);
+PrintArray(newArray);
+FindNum(newArray, myNum);
