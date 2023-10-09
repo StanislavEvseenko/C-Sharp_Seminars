c10a247b6293f19a93c4df816ee08f222745bd53
Stanislav <stanislav.evs.i@yandex.ru>
2023-10-09 20:01:53 +0300

    add Ex2

---
+++ Lesson6_Seminar/Example2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+// ввод трех чисел и проверка может ли быть треугольник со сторонами такой длины
+// теорема: каждая сторона треуголника меньше суммы двух других сторон
+
+
+Console.Clear();
+
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+bool IsTriangle(int a, int b, int c)
+{
+    return (a + b > c && b + c > a && a + c > b);
+}
+
+int firstSide = InputNum("Введите длину стороны А: ");
+int secondSide = InputNum("Введите длину стороны B: ");
+int thirdSide = InputNum("Введите длину стороны C: ");
+Console.WriteLine($"Возможность существования такого треугольника: {IsTriangle(firstSide, secondSide, thirdSide)}");
