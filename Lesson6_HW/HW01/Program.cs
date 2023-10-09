2f4538383fa74b57e917ba2c5a97e3d05f545fdf
Stanislav <stanislav.evs.i@yandex.ru>
2023-10-09 23:44:34 +0300

    add HW01

---
+++ Lesson6_HW/HW01/Program.cs
@@ -0,0 +1,25 @@
+﻿// пользователь вводит М чисел, посчитать сколько чисел будет больше 0
+Console.Clear();
+
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+void CheckForZero(int amount)
+{
+    int nums = 0;
+    for(int i = 0; i < amount; i++)
+    {
+        if (InputNum("Введите число: ") > 0)
+        {
+            nums++;
+        }
+    }
+    Console.WriteLine($"Положительных чисел: {nums}");
+}
+
+int amount = InputNum("Введите количество чисел: ");
+CheckForZero(amount);
