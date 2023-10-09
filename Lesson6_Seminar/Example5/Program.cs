06a070f1c4c85a38c6625a2d056fd9c516ed0a52
Stanislav <stanislav.evs.i@yandex.ru>
2023-10-09 23:04:01 +0300

    add Ex5

---
+++ Lesson6_Seminar/Example5/Program.cs
@@ -0,0 +1,13 @@
+﻿// копия массива поэлементно
+
+Console.Clear();
+
+int[] numbers = {1, 2, 3, 4, 5, 6, 7};
+int[] copy = new int[numbers.Length];
+for (int i = 0; i < numbers.Length; i++)
+{
+    copy[i] = numbers[i];
+}
+Console.WriteLine(string.Join(", ", copy));
+numbers[3] = 10;
+Console.WriteLine(string.Join(", ", numbers));
