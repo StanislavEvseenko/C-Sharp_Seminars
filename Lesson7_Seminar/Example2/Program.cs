686a389e98dd0ad6bb6fd46773fb3ab51058b4ff
Stanislav <stanislav.evs.i@yandex.ru>
2023-10-10 21:47:30 +0300

    add Ex2

---
+++ Lesson7_Seminar/Example2/Program.cs
@@ -0,0 +1,44 @@
+﻿// создание двумерного массива размера m на n. каждый элемент находится по формуле Amn = m + n
+// m = 3, n = 4
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+int[,] Create2DArray(int rows, int columns)
+{
+    return new int[rows, columns];
+}
+
+void Fill2DArray(int[,] ints, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < ints.GetLength(0); i++)
+        for (int j = 0; j < ints.GetLength(1); j++)
+            ints[i, j] = i + j;
+}
+
+void Print2DArray(int[,] ints)
+{
+    for (int i = 0; i < ints.GetLength(0); i++)
+    {
+        for (int j = 0; j < ints.GetLength(1); j++)
+            Console.Write(ints[i, j] + "\t");
+        Console.WriteLine();
+    }
+}
+
+int rows = InputNum("Введите количество строк: ");
+int cols = InputNum("Введите количество столбцов: ");
+int min = InputNum("Введите min: ");
+int max = InputNum("Введите max: ");
+
+int[,] anyArray = Create2DArray(rows, cols);
+Fill2DArray(anyArray, min, max);
+Print2DArray(anyArray);
