119e268a59ba4b3afc29278b4430c664bc1537e3
Stanislav <stanislav.evs.i@yandex.ru>
2023-10-13 03:31:56 +0300

    add Ex2

---
+++ Lesson8_Seminar/Example2/Program.cs
@@ -0,0 +1,63 @@
+﻿// создание двумерного массива размера m на n. 
+// замена строк и столбцов
+
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+int[,] Create2DArray(int rows, int columns)
+{
+    return new int[rows, columns];
+}
+
+void Fill2DArray(int[,] array, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = rnd.Next(minValue, maxValue);
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + "\t");
+        Console.WriteLine();
+    }
+}
+
+int[, ] ChangeArray(int[,] array)
+{
+    int[, ] newArray = new int[array.GetLength(0), array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            newArray[i, j] = array[j, i];
+    return newArray;
+}
+
+bool Test(int[, ] array)
+{
+    return array.GetLength(0) == array.GetLength(1);
+}
+
+int rows = InputNum("Введите количество строк: ");
+int cols = InputNum("Введите количество столбцов: ");
+int min = InputNum("Введите min: ");
+int max = InputNum("Введите max: ");
+
+int[,] anyArray = Create2DArray(rows, cols);
+Fill2DArray(anyArray, min, max);
+Print2DArray(anyArray);
+Console.WriteLine();
+if (Test(anyArray))
+{
+    int[,] reverseArray = ChangeArray(anyArray);
+    Print2DArray(reverseArray);
+} else {
+    Console.WriteLine("Массив не квадратный!");
+}
