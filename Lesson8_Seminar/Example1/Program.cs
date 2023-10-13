c982543a889e58b7c50c6c0ec800842f9d6e2970
Stanislav <stanislav.evs.i@yandex.ru>
2023-10-13 03:13:09 +0300

    add Ex1

---
+++ Lesson8_Seminar/Example1/Program.cs
@@ -0,0 +1,55 @@
+﻿// создание двумерного массива размера m на n. 
+// найти сумму элементов главной диагонали
+
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+int[,] Create2DArray(int rows, int columns)
+{
+    return new int[rows, columns];
+}
+
+void Fill2DArray(int[,] ints, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < ints.GetLength(0); i++)
+        for (int j = 0; j < ints.GetLength(1); j++)
+            ints[i, j] = rnd.Next(minValue, maxValue);
+}
+
+void Print2DArray(int[,] ints)
+{
+    for (int i = 0; i < ints.GetLength(0); i++)
+    {
+        for (int j = 0; j < ints.GetLength(1); j++)
+            Console.Write(ints[i, j] + "\t");
+        Console.WriteLine();
+    }
+}
+
+void ReverseRows(int[,] array)
+{
+    int bufer = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        bufer = array[0, i];
+        array[0, i] = array[array.GetLength(0) - 1, i];
+        array[array.GetLength(0) - 1, i] = bufer;
+    }
+}
+
+int rows = InputNum("Введите количество строк: ");
+int cols = InputNum("Введите количество столбцов: ");
+int min = InputNum("Введите min: ");
+int max = InputNum("Введите max: ");
+
+int[,] anyArray = Create2DArray(rows, cols);
+Fill2DArray(anyArray, min, max);
+Print2DArray(anyArray);
+ReverseRows(anyArray);
+Console.WriteLine();
+Print2DArray(anyArray);
