071a621f584207a843e8ac0f274722c75cfa34c0
Stanislav <stanislav.evs.i@yandex.ru>
2023-10-18 15:02:11 +0300

    add EX3

---
+++ Lesson9_Seminar/Example3/Program.cs
@@ -0,0 +1,23 @@
+﻿// РЕКУРСИЯ
+// Задайте число M.
+// вывод суммы цифр этого числа.
+
+
+Console.Clear();
+
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+int SumDigits(int number)
+{
+    if (number == 0) return 0;
+    return SumDigits(number / 10) + number%10;
+}
+
+int num = InputNum("Введите число: ");
+int sum = SumDigits(num);
+Console.WriteLine(sum);
