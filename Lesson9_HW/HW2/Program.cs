8ab30abbf546c4e9945a225d86e012862fe6c055
Stanislav <stanislav.evs.i@yandex.ru>
2023-10-18 17:49:04 +0300

    add HW2

---
+++ Lesson9_HW/HW2/Program.cs
@@ -0,0 +1,24 @@
+﻿// РЕКУРСИЯ
+// Задайте значение M и N.
+// вывод суммы всех натуральных чисел от M до N.
+
+
+Console.Clear();
+
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+int SumDigits(int m, int n)
+{
+    if (m == n) return m;
+    return SumDigits(m + 1, n) + m;
+}
+
+int m = InputNum("Введите число M: ");
+int n = InputNum("Введите число N: ");
+int sum = SumDigits(m, n);
+Console.WriteLine(sum);
