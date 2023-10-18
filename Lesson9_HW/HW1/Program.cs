a1d3470918f55a97e51777630c4ecb4ddf2ba2e5
Stanislav <stanislav.evs.i@yandex.ru>
2023-10-18 15:52:35 +0300

    add HW1

---
+++ Lesson9_HW/HW1/Program.cs
@@ -0,0 +1,25 @@
+﻿// РЕКУРСИЯ
+// Задайте значение N.
+// вывод всех натуральных чисел от N до 1.
+
+
+Console.Clear();
+
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+void PrintNumbers(int n)
+{
+    if(n > 0)
+    {
+        Console.Write(n + " ");
+        PrintNumbers(n - 1);
+    }
+}
+
+int number = InputNum("Введите число: ");
+PrintNumbers(number);
