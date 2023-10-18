7884f77ada6b78e29357f4aacb113546d047b08c
Stanislav <stanislav.evs.i@yandex.ru>
2023-10-18 15:37:45 +0300

    add EX4

---
+++ Lesson9_Seminar/Example4/Program.cs
@@ -0,0 +1,29 @@
+﻿// РЕКУРСИЯ
+// Задайте два числа A и B.
+// возвести число A в степень B.
+
+
+Console.Clear();
+
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+double Powered(int num, int degree)
+{
+    if (degree == 0)
+        return 1;
+    if (degree > 0) {
+        return num * Powered(num, --degree);
+    } else {
+        return 1.0 / num * Powered(num, ++degree);
+    }
+}
+
+int numA = InputNum("Введите число A: ");
+int numB = InputNum("Введите число B: ");
+
+Console.WriteLine(Powered(numA, numB));
