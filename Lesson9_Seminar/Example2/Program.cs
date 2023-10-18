dc05c437c624658767cbf4e5cedd5b585462b5fb
Stanislav <stanislav.evs.i@yandex.ru>
2023-10-18 14:47:34 +0300

    add EX2

---
+++ Lesson9_Seminar/Example2/Program.cs
@@ -0,0 +1,28 @@
+﻿// РЕКУРСИЯ
+// Задайте значение M и N.
+// вывод всех натуральных чисел от M до N.
+
+
+Console.Clear();
+
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+void PrintNumbers(int m, int n)
+{
+    if (m == n)
+    {
+        Console.Write(m + " ");
+        return;
+    }
+    PrintNumbers(m, n - 1);
+    Console.Write(n + " ");
+}
+
+int m = InputNum("Введите число M: ");
+int n = InputNum("Введите число N: ");
+PrintNumbers(m, n);
