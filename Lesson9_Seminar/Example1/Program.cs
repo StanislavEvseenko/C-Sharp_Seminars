448c4dec45c205fc8ecada1aadf7de1945d38d44
Stanislav <stanislav.evs.i@yandex.ru>
2023-10-18 14:28:28 +0300

    add EX1

---
+++ Lesson9_Seminar/Example1/Program.cs
@@ -0,0 +1,33 @@
+﻿// РЕКУРСИЯ
+// Задайте значение N.
+// вывод всех натуральных чисел от 1 до N.
+
+
+Console.Clear();
+
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+// void PrintNumbers(int n)
+// {
+//     if(n > 0)
+//     {
+//         PrintNumbers(n - 1);
+//         Console.Write(n + " ");
+//     }
+// }
+
+string PrintNumbers(int n, int start)
+{
+    if(start == n)
+        return n.ToString();
+    return start + " " + PrintNumbers(n, start + 1);
+}
+
+int number = InputNum("Введите число: ");
+Console.WriteLine(PrintNumbers(number, 1));
+// PrintNumbers(number);
